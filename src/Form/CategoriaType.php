b29447417c4e874c04858a398ddcda6af356bc8b
Gleiciane Galdino <gleiciane.galdino@demprel.local>
2023-04-28 17:02:39 -0300

    Validações

--- src/Form/CategoriaType.php
+++ src/Form/CategoriaType.php
@@ -7,14 +7,33 @@ use Symfony\Component\Form\AbstractType;
 use Symfony\Component\Form\Extension\Core\Type\SubmitType;
 use Symfony\Component\Form\Extension\Core\Type\TextType;
 use Symfony\Component\Form\FormBuilderInterface;
+use Symfony\Component\Validator\Constraints\Length;
+use Symfony\Component\Validator\Constraints\NotBlank;
+//use Symfony\Component\Validator\Constraints\NotBlank;
+//use Symfony\Component\Validator\Constraints as Assert;
+
+
 
 class CategoriaType extends AbstractType
 {
     public function buildForm(FormBuilderInterface $builder, array $options )
     {
         $builder
-        ->add('descricaocategoria', TextType::class, 
-        ['label' => 'Descrição da categoria: '])
+        ->add('descricaocategoria', TextType::class, [
+             'required' => false,
+            'label' => 'Descrição da categoria: ',
+            'constraints' => [
+                new Length([
+                    'min' => 5,
+                    'max' => 50,
+                    'minMessage' => 'O mínimo de caracteres é: {{ limit }} ',
+                    'maxMessage' => 'O maximo de caracteres é: {{ limit }} ',
+                 ]),
+                 new NotBlank([
+                    'message' => 'Não pode ser nulo.'
+                 ])
+                ]
+        ])
         ->add('Salvar', SubmitType::class);
     }
 }
--- src/Form/ProdutoType.php
+++ src/Form/ProdutoType.php
@@ -8,6 +8,10 @@ use Symfony\Component\Form\AbstractType;
 use Symfony\Component\Form\Extension\Core\Type\TextType;
 use Symfony\Component\Form\Extension\Core\Type\SubmitType;
 use Symfony\Component\Form\FormBuilderInterface;
+use Symfony\Component\Validator\Constraints\Length;
+use Symfony\Component\Validator\Constraints\NotBlank;
+
+
 
 
 class ProdutoType extends AbstractType
@@ -15,8 +19,21 @@ class ProdutoType extends AbstractType
     public function  buildForm(FormBuilderInterface $builder, array $options )
     {
         $builder
-            ->add('nomeproduto', TextType::class, 
-            ['label' => 'Nome do produto: '])
+            ->add('nomeproduto', TextType::class, [
+                'required' => false,
+               'label' => 'Nome do Produto: ',
+               'constraints' => [
+                   new Length([
+                       'min' => 5,
+                       'max' => 50,
+                       'minMessage' => 'O mínimo de caracteres é: {{ limit }} ',
+                       'maxMessage' => 'O maximo de caracteres é: {{ limit }} ',
+                    ]),
+                    new NotBlank([
+                       'message' => 'Não pode ser nulo.'
+                    ])
+                   ]
+           ])
             
             ->add('valor', TextType::class, ['label' => 'Valor: '])
 
